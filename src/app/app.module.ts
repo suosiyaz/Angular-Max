f003cd8ec8d391bb8a99bd335b0a708d6468bfeb
suosiyaz <siyazputhancode@gmail.com>
2019-07-25 11:11:54 +0530

    5 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     RecipeItemComponent, 
     ShoppingListComponent, 
     ShoppingEditComponent, 
-    DropdownDirective, RecipeStartComponent 
+    DropdownDirective, RecipeStartComponent, RecipeEditComponent 
   ],
   bootstrap: [ 
     AppComponent 
