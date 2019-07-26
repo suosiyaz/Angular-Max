a1685db39a3d800bd2ac21caea8971b81c1575ee
suosiyaz <siyazputhancode@gmail.com>
2019-07-26 13:30:57 +0530

    4 files modified

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable()
 export class DataStorageService {
@@ -12,6 +13,13 @@ export class DataStorageService {
     this.http.put('https://angular-max-41709.firebaseio.com/recipes.json', recipes)
       .subscribe(response => {
         console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://angular-max-41709.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
       });
   }
 
