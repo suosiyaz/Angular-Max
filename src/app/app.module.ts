a53c7adfe157041ee84fa1ad55896d9fa48e05c4
suosiyaz <siyazputhancode@gmail.com>
2019-09-05 13:42:42 +0530

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { DataStorageService } from './shared/data-storage.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeResolverService } from './recipes/recipe-resolver.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthService } from './auth/auth.service';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    AuthModule,
+    SharedModule,
+    CoreModule
   ],
   declarations: [ 
     AppComponent, 
     HeaderComponent, 
-    AuthComponent 
   ],
   bootstrap: [ 
     AppComponent 
-  ],
-  providers: [
-    RecipeService, 
-    ShoppingListService, 
-    DataStorageService, 
-    RecipeResolverService, 
-    AuthService, 
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
   ]
 })
 export class AppModule { }
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { RecipeService } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { DataStorageService } from './shared/data-storage.service';
+import { RecipeResolverService } from './recipes/recipe-resolver.service';
+import { AuthService } from './auth/auth.service';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    RecipeService, 
+    ShoppingListService, 
+    DataStorageService, 
+    RecipeResolverService, 
+    AuthService, 
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+  ]
 })
 export class CoreModule { }
